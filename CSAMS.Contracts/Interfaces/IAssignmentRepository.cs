2544f7f86e429d82f438fb0cac28b58e9ea7380c
Svein Are Danielsen <svein.danielsen@inmeta.no>
2020-01-24 11:25:30 +0100

    added interface for assignemtn repository

---
+++ CSAMS.Contracts/Interfaces/IAssignmentRepository.cs
@@ -0,0 +1,9 @@
+﻿using CSAMS.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSAMS.Contracts.Interfaces {
+    public interface IAssignmentRepository : IAsyncRepository<Assignment> {
+    }
+}
