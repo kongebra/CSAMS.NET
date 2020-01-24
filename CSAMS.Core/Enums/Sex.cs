f9be93dbbedaf94bdb0e4ae7a643f8dc59ff192c
Svein Are Danielsen <svein.danielsen@inmeta.no>
2020-01-24 08:47:56 +0100

    added enums for user role and sex (users)

---
+++ CSAMS.Core/Enums/Sex.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSAMS.Core.Enums {
+    public enum Sex {
+        Undefined = 0,
+        Male,
+        Female,
+    }
+}
---
+++ CSAMS.Core/Enums/UserRole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSAMS.Core.Enums {
+    public enum UserRole {
+        Student = 0,
+        TeacherAssistant,
+        Teacher,
+    }
+}
